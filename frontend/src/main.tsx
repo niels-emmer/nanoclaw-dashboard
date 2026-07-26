1d9bfcec4eef77c024175a0bc81218d4a401a295
Niels Emmer <n.emmer@teamrockstars.nl>
2026-07-26 05:41:54 +0200

    Enable HeroUI dark mode on html element

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { useTheme } from '@heroui/react'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function Root() {
+  useTheme('dark')
+  return (
+    <StrictMode>
+      <App />
+    </StrictMode>
+  )
+}
+
+createRoot(document.getElementById('root')!).render(<Root />)
